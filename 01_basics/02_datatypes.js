6d24e330ad0b2d53941063ec4cc9f1f330b58a1d
Goutamkr07 <149079767+Goutamkr07@users.noreply.github.com>
2024-07-09 18:41:13 +0000

    daratypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "Goutam"
+let age = 22
+let isLoggedIn = true
+
+// number => 2 to the power 53
+// bigint for larg values
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => unique
+
+
+// object
+
+
+// typeof => tell the datatype of the variable 
+
+console.log(typeof age)
+console.log(typeof null) // output => object
+console.log(typeof undefined) // output => undefined
