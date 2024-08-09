865a03b96813aedc510545b9d8f6e9fac259b2ed
Goutamkr07 <149079767+Goutamkr07@users.noreply.github.com>
2024-08-09 09:14:55 +0000

    Convertion of datatypes

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -1,4 +1,4 @@
-let score = "33" //output => 33
+// let score = "33" //output => 33
 // let score = "33abc" //output => NaN (Not a Number)
 // let score = true //output => 1
 // let score = false //output => 0
@@ -6,19 +6,19 @@ let score = "33" //output => 33
 // console.log(typeof score);
 // console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-let isLoggedIn = 1;
+// let isLoggedIn = 1;
 
 // let isLoggedIn = 0;
 // let isLoggedIn = "";
 // let isLoggedIn = "goutam";
 
 
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
 
 /*      output
 
@@ -29,8 +29,39 @@ console.log(booleanisLoggedIn);
 
 
 // string convertion
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// ************* Operations ******
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //2^3 (power)
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "hello "
+// let str2 = "goutam"
+
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+
