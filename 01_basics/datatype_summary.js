b74386864bf57f7974d075e0c9e1f5350f3c4928
Goutam Sharma <149079767+Goutamkr07@users.noreply.github.com>
2024-09-09 10:14:15 +0000

    datatype_summary

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -3,20 +3,27 @@
 
 // 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
+//number
 const score = 100
 const scoreValue = 100.3
 
+//boolean
 const isLoggedIn = false
+
+//null
 const outsideTemp = null
+
+//undefined
 let userEmail;
 
-
+//symbol
 const id = Symbol('123')
 const anotherId = Symbol('123')
-
+//compare
 console.log(id === anotherId);
 
-const bigNumber = 777777777771323982734977483n
+//bigint
+const bigNumber = 7777777713239827349774836667n
 
 // *********************************
 
@@ -24,15 +31,18 @@ const bigNumber = 777777777771323982734977483n
 
 // Array, Object, Function
 
+//array
 const heros = ["Shk=aktiman","naagraj", "doga"]
 
+//object
 let myObj = {
     name: "goutam",
     age: 22,
 }
 
+//function
 const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof bigNumber);
+console.log(typeof bigNumber); // to find the datatype
